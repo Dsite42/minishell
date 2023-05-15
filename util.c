3454ce500f6b8b72e1ecf6f20c056eafa3b449bf
jsprenge <jsprenge@student.42wolfsburg.de>
2023-05-15 18:21:40 +0200

    Add various slice functions

--- util.c
+++ util.c
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:20:44 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/05/15 16:21:20 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/05/15 18:20:09 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,67 @@ char	*slice_to_str(t_slice slice)
 	memcpy(new_string, slice.data, slice.size);
 	new_string[slice.size] = '\0';
 	return (new_string);
+}
+
+int	slice_str_equal(t_slice slice, const char *string)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < slice.size && string[index] != '\0')
+	{
+		if (slice.data[index] != string[index])
+			return (0);
+		index++;
+	}
+	if (index == slice.size && string[index] == '\0')
+		return (1);
+	return (0);
+}
+
+t_slice	advance(t_slice slice)
+{
+	if (slice.size > 0)
+	{
+		slice.data++;
+		slice.size--;
+	}
+	return (slice);
+}
+
+void	split_once(t_slice slice, int (*predicate)(char),
+			t_slice *p_part0, t_slice *p_part1)
+{
+	t_slice	part0;
+	t_slice	part1;
+
+	part0.data = slice.data;
+	part0.size = 0;
+	while (part0.size < slice.size)
+	{
+		if (predicate(slice.data[part0.size]))
+			break ;
+		part0.size++;
+	}
+	part1.data = &slice.data[part0.size];
+	part1.size = slice.size - part0.size;
+	if (p_part0 != NULL)
+		*p_part0 = part0;
+	if (p_part1 != NULL)
+		*p_part1 = part1;
+}
+
+t_slice	trim_left(t_slice slice, int (*predicate)(char))
+{
+	size_t	index;
+
+	index = 0;
+	while (index < slice.size)
+	{
+		if (predicate(slice.data[index]))
+			break ;
+	}
+	slice.data += index;
+	slice.size -= index;
+	return (slice);
 }
--- util.h
+++ util.h
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:15:52 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/05/15 16:16:24 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/05/15 18:17:44 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,11 @@ typedef struct s_slice
 
 t_slice	slice0(const char *string);
 char	*slice_to_str(t_slice slice);
+int		slice_str_equal(t_slice slice, const char *string);
+
+t_slice	advance(t_slice slice);
+void	split_once(t_slice slice, int (*predicate)(char),
+			t_slice *p_part0, t_slice *p_part1);
+t_slice	trim_left(t_slice slice, int (*predicate)(char));
 
 #endif // !UTIL_H
