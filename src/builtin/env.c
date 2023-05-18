8a0b53d096d6d667f6f2d4778537d9c9958ab22f
jsprenge <jsprenge@student.42wolfsburg.de>
2023-05-18 16:51:33 +0200

    Rename root_var to head_var for iteration in builtin_env

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 13:49:38 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/05/18 16:37:51 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/05/18 16:50:57 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	builtin_env(int argc, char *argv[], int out_fd, t_state *state)
 {
-	t_var	*root_var;
+	t_var	*head_var;
 
 	(void) argc;
 	(void) argv;
-	root_var = state->root_var;
-	while (root_var != NULL)
+	head_var = state->root_var;
+	while (head_var != NULL)
 	{
-		if (root_var->flags & VAR_EXPORT)
-			print_fd(out_fd, "%s=%s\n", root_var->name, root_var->value);
-		root_var = root_var->next;
+		if (head_var->flags & VAR_EXPORT)
+			print_fd(out_fd, "%s=%s\n", head_var->name, head_var->value);
+		head_var = head_var->next;
 	}
 	return (0);
 }
