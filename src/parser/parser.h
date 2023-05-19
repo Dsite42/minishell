c80675640b77bd8d6feb454942749ed0ac7cd725
jsprenge <jsprenge@student.42wolfsburg.de>
2023-05-19 23:02:33 +0200

    Introduce and document t_word structure

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,11 +6,41 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 18:55:33 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/05/19 18:56:30 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/05/19 23:01:17 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
+
+# include "../util/util.h"
+
+// Words are organized into groups of chains, eg. the following command ...
+//    echo -n "Hello, $NAME, nice to meet you."
+//
+// ... is parsed into the following structure of words:
+//    ["echo"] -> next_group -> ["-n"] -> next_group -> [      "Hello, "      ]
+//                                                                 |
+//                                                            next_chain
+//                                                                 |
+//                                                      [       "NAME"        ]
+//                                                                 |
+//                                                            next_chain
+//                                                                 |
+//                                                      [", nice to meet you."]
+
+# define WORD_VAR 1
+
+typedef struct s_word
+{
+	struct s_word	*next_group;
+	struct s_word	*next_chain;
+	unsigned int	flags;
+	t_slice			slice;
+	void			*cache;
+}	t_word;
+
+// NOTE: t_word.next_group is only valid for the first node of a chain
+// NOTE: The validity of t_word.cache is up to functions writing to it
 
 #endif // !PARSER_H
