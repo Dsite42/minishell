9838122fd0fa2735a329a139eabf7dd6d3561774
jsprenge <jsprenge@student.42wolfsburg.de>
2023-05-22 16:06:54 +0200

    Simplify t_builder API

--- src/parser/builder.c
+++ src/parser/builder.c
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 02:43:48 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/05/22 03:17:15 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/05/22 15:53:39 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 #include <stdlib.h>
 
-int	builder_append_group(t_builder *builder, unsigned int flags, t_slice slice)
+static int	builder_append_group(
+	t_builder *builder, unsigned int flags, t_slice slice)
 {
 	t_word	*new_word;
 
@@ -29,7 +30,8 @@ int	builder_append_group(t_builder *builder, unsigned int flags, t_slice slice)
 	return (1);
 }
 
-int	builder_append_chain(t_builder *builder, unsigned int flags, t_slice slice)
+static int	builder_append_chain(
+	t_builder *builder, unsigned int flags, t_slice slice)
 {
 	t_word	*new_word;
 
@@ -43,4 +45,25 @@ int	builder_append_chain(t_builder *builder, unsigned int flags, t_slice slice)
 		builder->chain_head_word->next_chain = new_word;
 	builder->chain_head_word = new_word;
 	return (1);
+}
+
+void	builder_group(t_builder *builder)
+{
+	builder->is_new_group = 1;
+}
+
+int	builder_append(t_builder *builder, unsigned int flags, t_slice slice)
+{
+	int	result;
+
+	if (slice.size == 0)
+		return (1);
+	if (builder->is_new_group)
+	{
+		result = builder_append_group(builder, flags, slice);
+		if (result)
+			builder->is_new_group = 0;
+		return (result);
+	}
+	return (builder_append_chain(builder, flags, slice));
 }
--- src/parser/private.h
+++ src/parser/private.h
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 02:11:15 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/05/22 03:17:03 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/05/22 15:31:18 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,18 @@ typedef struct s_builder
 	t_word	*root_word;
 	t_word	*group_head_word;
 	t_word	*chain_head_word;
+	int		is_new_group;
 }	t_builder;
 
 // builder.c
-int	builder_append_group(t_builder *builder, unsigned int flags, t_slice slice);
-int	builder_append_chain(t_builder *builder, unsigned int flags, t_slice slice);
+void	builder_group(t_builder *builder);
+int		builder_append(t_builder *builder, unsigned int flags, t_slice slice);
 
 // grammar.c
-int	is_space(char input);
-int	is_not_identifier(char input);
-int	is_word_split(char input);
-int	is_single_quote_split(char input);
-int	is_double_quote_split(char input);
+int		is_space(char input);
+int		is_not_identifier(char input);
+int		is_word_split(char input);
+int		is_single_quote_split(char input);
+int		is_double_quote_split(char input);
 
 #endif // !PRIVATE_H
