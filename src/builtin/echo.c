aae387b620858a07c1293c056288bafa97dedd6e
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-05-22 23:15:37 +0200

    implement print_folder_content

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -12,9 +12,14 @@
 
 #include "builtin.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <sys/param.h>
 #include "../libft.h"
 
 // ft_split is used to imitate the parser
+// print_folder_content is for fun and has no proper error handling
 
 int	arr_len(char **arr)
 {
@@ -28,10 +33,32 @@ int	arr_len(char **arr)
 	return (i);
 }
 
+
+void	print_folder_content(int n_flag)
+{
+	DIR						*dir_stream;
+	struct dirent			*directory_entry;
+	struct stat	*restrict	statbuf;
+	char					buffer[MAXPATHLEN + 1];
+
+	dir_stream = opendir(getcwd(buffer, sizeof(buffer)));
+	directory_entry = readdir(dir_stream);
+	statbuf = malloc(sizeof(struct stat));
+	while (directory_entry != NULL)
+	{
+		lstat(ft_strjoin(buffer, ft_strjoin("/", directory_entry->d_name)), statbuf);
+		if (statbuf->st_mode & __S_IFREG || statbuf->st_mode & __S_IFDIR)
+			printf("%s ", directory_entry->d_name);
+		directory_entry = readdir(dir_stream);
+	}
+	if (n_flag == 0)
+		printf("\n");
+}
+
 int	builtin_echo(int argc, char *argv[], int out_fd, t_state *state)
 {
-	char	**splitted_argvs;
-	int		n_flag;
+	char			**splitted_argvs;
+	int				n_flag;
 
 	n_flag = 0;
 	splitted_argvs = ft_split(argv[2], ' ');
@@ -50,6 +77,12 @@ int	builtin_echo(int argc, char *argv[], int out_fd, t_state *state)
 	}
 	if (n_flag == 1)
 		splitted_argvs++;
+	if ((n_flag == 1 && argc == 2 || n_flag == 0 && argc == 1)
+		&& **splitted_argvs == '*')
+	{
+		print_folder_content(n_flag);
+		return (0);
+	}
 	while (*splitted_argvs)
 	{
 		print_fd(out_fd, "%s", *splitted_argvs);
