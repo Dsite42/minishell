e87807aa58b04d3a8c9ca50ca5c07460c9daa2e9
jsprenge <jsprenge@student.42wolfsburg.de>
2023-05-22 23:00:33 +0200

    Modify begin_word_split() to recognize redirection and pipe operators

--- src/parser/grammar.c
+++ src/parser/grammar.c
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 02:14:45 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/05/22 22:27:55 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/05/22 22:59:12 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,19 @@ size_t	begin_not_identifier(t_slice slice)
 
 size_t	begin_word_split(t_slice slice)
 {
+	size_t	length;
+
 	if (slice.size == 0)
 		return (0);
+	length = slice_str_begin(slice, "<<");
+	if (length != 0)
+		return (length);
+	length = slice_str_begin(slice, ">>");
+	if (length != 0)
+		return (length);
 	return (begin_space(slice) || *slice.data == '\'' || *slice.data == '"'
-		|| *slice.data == '$');
+		|| *slice.data == '$' || *slice.data == '<' || *slice.data == '>'
+		|| *slice.data == '|');
 }
 
 size_t	begin_single_quote_split(t_slice slice)
