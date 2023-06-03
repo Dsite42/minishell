869157c1d26c0cf85b90b8d6c7d4bfb6d3e0f852
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-06-03 20:18:27 +0200

    implement ctrl-signals into new master with parser

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:10:00 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/06/03 20:12:51 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/06/03 20:16:36 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "state/state.h"
 #include "parser/parser.h"
 #include "builtin/builtin.h"
+#include <signal.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,13 +26,38 @@
 
 static void	dump_argv(char **argv, t_state *state)
 {
-  size_t	index;
-	index = 0;
-	while (argv[index] != NULL)
+	if (ms_strncmp(*argv, "export", 6) == 0)
 	{
-		print_fd(STDOUT_FILENO, "%u: %s\n", index, argv[index]);
-		index++;
+		builtin_export(0, argv, 0, state);
 	}
+	if (ms_strncmp(*argv, "env", 3) == 0)
+	{
+		builtin_env(0, argv, 0, state);
+	}
+	if (ms_strncmp(*argv, "echo", 4) == 0)
+	{
+		builtin_echo(0, argv, 0, state);
+	}
+	if (ms_strncmp(*argv, "exit", 4) == 0)
+	{
+		builtin_exit(0, argv, 0, state);
+	}
+	if (ms_strncmp(*argv, "cd", 2) == 0)
+	{
+		builtin_cd(0, argv, 0, state);
+	}
+	if (ms_strncmp(*argv, "unset", 5) == 0)
+	{
+		//builtin_unset(0, argv, 0, state);
+	}
+
+  	//size_t	index;
+	//index = 0;
+	//while (argv[index] != NULL)
+	//{
+	//	print_fd(STDOUT_FILENO, "%u: %s\n", index, argv[index]);
+	//	index++;
+	//}
 }
 
 static t_result	handle_line(char *line, t_state *state)
@@ -57,17 +83,34 @@ static t_result	handle_line(char *line, t_state *state)
 	return (S_OK);
 }
 
+// ctrl-\ does anyway nothing.
+void	handle_signals(int sig)
+{
+	(void)sig;
+	if (sig == SIGINT)
+		write(0, "\nminishell> ", 12);
+	else if (sig == SIGTSTP)
+	{
+		//vars_clr(&state.root_var); should be state as global?
+		rl_clear_history();
+		exit(0);
+	}
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_state		state;
 	t_result	result;
 	char		*line;
+	struct sigaction	sa;
 
 	(void) argc;
 	(void) argv;
-	
-if (!vars_from_envp(envp, &state.root_var))
+	if (!vars_from_envp(envp, &state.root_var))
 		return (1);
+	sa.sa_handler = &handle_signals;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGTSTP, &sa, NULL);
 	while (1)
 	{
 		line = readline("minishell> ");
