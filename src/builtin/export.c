89225a2c83963814b6c56486767a46307514251a
jsprenge <jsprenge@student.42wolfsburg.de>
2023-06-04 22:10:29 +0200

    Update argument handling in export builtin

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 10:33:06 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/06/03 10:34:39 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/06/04 22:05:00 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 #include "../util/util.h"
 #include "../state/state.h"
+#include "../parser/parser.h"
 
-int	print_exports(t_var *var, int out_fd)
+#include <unistd.h>
+
+static int	print_exports(t_var *var, int out_fd)
 {
 	if (var == NULL)
 		return (0);
@@ -27,42 +30,45 @@ int	print_exports(t_var *var, int out_fd)
 	return (0);
 }
 
-int	export_vars(char *argv[], t_state *state)
+// TODO: Edge-cases with empty values, etc, etc
+static int	set_export(t_state *state, const char *argument)
 {
+	t_var	*var;
 	t_slice	name;
 	t_slice	value;
-	t_var	*var;
+	t_slice	remainder;
 
-	split_once(slice0(*argv), begin_delimiter, &name, &value);
+	split_once(slice0(argument), begin_delimiter, &name, &value);
 	value = advance(value);
+	split_once(name, begin_not_identifier, &name, &remainder);
+	if (name.size == 0 || remainder.size > 0)
+	{
+		print_fd(STDERR_FILENO,
+			"minishell: export: `%s': not a valid identifier\n", argument);
+		return (0);
+	}
 	var = vars_set(&state->root_var, name, value);
 	if (var == NULL)
-		return (1);
+		return (0);
 	var->flags |= VAR_EXPORT;
-	return (0);
+	return (1);
 }
 
 int	builtin_export(int argc, char *argv[], int out_fd, t_state *state)
 {
-	int		error_detected;
+	int	index;
+	int	return_code;
 
-	(void) argc;
-	argv++;
-	error_detected = 0;
-	if (argv[0] == NULL)
+	(void) out_fd;
+	if (argc == 1)
 		return (print_exports(state->root_var, out_fd));
-	while (*argv != NULL)
+	index = 1;
+	return_code = 0;
+	while (index < argc)
 	{
-		if (!((ms_isalpha(**argv)) || **argv == '_')
-			|| ms_strchr(*argv, '=') == NULL)
-		{
-			print_fd(out_fd,
-				"minishell: export: `%s': not a valid identifier\n", *argv);
-			error_detected = 1;
-		}
-		else
-			error_detected = export_vars(argv, state);
-		argv++;
+		if (!set_export(state, argv[index]))
+			return_code = 1;
+		index++;
 	}
-	return (error_detected);
+	return (return_code);
 }
