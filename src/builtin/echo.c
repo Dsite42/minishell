00e0522b75af0072c0ac122eeffcb57d2fa83538
cgodecke <cgodecke@student.42wolfsburg.de>
2023-06-07 15:24:56 +0200

    solved issue #17. multible -n arguments are correctly handled now

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,42 +6,26 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 16:58:05 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/06/07 15:12:53 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/06/07 15:22:18 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-static void	is_print_no_new_line(int argc, char ***argv, int *n_flag)
-{
-	if (argc != 0)
-	{
-		while (ms_strlen(**argv) == 2
-			&& ms_strncmp(**argv, "-n\0", 3) == 0)
-		{
-			*n_flag = 1;
-			*argv = *argv + 1;
-		}
-	}
-}
-
 int	builtin_echo(int argc, char *argv[], int out_fd, t_state *state)
 {
 	int	index;
 	int	print_newline;
-	int n_flag;
 
 	(void) state;
-	n_flag = 0;
-	argv++;
-	argc = ms_arr_len(argv);
-	is_print_no_new_line(argc, &argv, &n_flag);
-	if (argc == 0 || (argc == 1 && n_flag == 1))
+	print_newline = 1;
+	index = 1;
+	while (argc >= 2 && ms_str_compare("-n", argv[index], 0) == 0)
 	{
-		print_newline = 1;
-		index = 1;
+		print_newline = 0;
+		index++;
 	}
-	while (*argv)
+	while (index < argc)
 	{
 		print_fd(out_fd, "%s", argv[index]);
 		if (index != argc - 1)
