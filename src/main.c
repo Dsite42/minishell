049b64dbebf30063d2d680f6dffe544556befb83
jsprenge <jsprenge@student.42wolfsburg.de>
2023-06-29 18:36:16 +0200

    Clean up main for testing insert_vars

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: jsprenge <jsprenge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 16:10:00 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/06/23 01:51:41 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/06/29 18:34:42 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,80 +23,72 @@
 
 #include <string.h>
 
-#define FLAGS "VO---D"
+#define FLAGS "VO---QP"
+
+static void	dump_flags(char flag_prefix[sizeof(FLAGS)], unsigned int flags)
+{
+	unsigned int	bit;
+
+	ms_copy(flag_prefix, FLAGS, sizeof(FLAGS));
+	bit = 0;
+	while (bit < sizeof(FLAGS) - 1)
+	{
+		if ((flags & (1 << bit)) == 0 && (bit < 2 || bit > 4))
+			flag_prefix[bit] = '-';
+		bit++;
+	}
+	if ((flags & WORD_OP_MASK) == WORD_OP_PIPE)
+		ms_copy(&flag_prefix[2], "(P)", 3);
+	if ((flags & WORD_OP_MASK) == WORD_OP_READ)
+		ms_copy(&flag_prefix[2], "(R)", 3);
+	if ((flags & WORD_OP_MASK) == WORD_OP_WRITE)
+		ms_copy(&flag_prefix[2], "(W)", 3);
+	if ((flags & WORD_OP_MASK) == WORD_OP_APPEND)
+		ms_copy(&flag_prefix[2], "(A)", 3);
+	if ((flags & WORD_OP_MASK) == WORD_OP_HEREDOC)
+		ms_copy(&flag_prefix[2], "(H)", 3);
+}
+
+static void	dump_word(t_word *head_group, t_word *head_chain)
+{
+	const char	*join_prefix;
+	char		flag_prefix[sizeof(FLAGS)];
+
+	dump_flags(flag_prefix, head_chain->flags);
+	if (head_chain == head_group && head_group->next_chain == NULL)
+	{
+		print_fd(STDOUT_FILENO, "  [%s] '%a'\n", flag_prefix, head_chain->slice);
+		return ;
+	}
+	if (head_chain == head_group)
+		join_prefix = "╭ ";
+	else if (head_chain->next_chain == NULL)
+		join_prefix = "╰ ";
+	else
+		join_prefix = "│ ";
+	print_fd(STDOUT_FILENO, "%s[%s] '%a'\n",
+		join_prefix, flag_prefix, head_chain->slice);
+}
 
 static void	dump_words(t_word *head_group)
 {
-	t_word		*head_chain;
-	const char	*join_prefix;
-	char		flag_prefix[sizeof(FLAGS)];
+	t_word	*head_chain;
 
 	while (head_group != NULL)
 	{
 		head_chain = head_group;
 		while (head_chain != NULL)
 		{
-			ms_copy(flag_prefix, FLAGS, sizeof(FLAGS));
-			for (unsigned int bit = 0; bit < sizeof(FLAGS) - 1; bit++)
-				if ((head_chain->flags & (1 << bit)) == 0 && (bit < 2 || bit > 4))
-					flag_prefix[bit] = '-';
-			if ((head_chain->flags & WORD_OP_MASK) == WORD_OP_PIPE)
-				ms_copy(&flag_prefix[2], "(P)", 3);
-			if ((head_chain->flags & WORD_OP_MASK) == WORD_OP_READ)
-				ms_copy(&flag_prefix[2], "(R)", 3);
-			if ((head_chain->flags & WORD_OP_MASK) == WORD_OP_WRITE)
-				ms_copy(&flag_prefix[2], "(W)", 3);
-			if ((head_chain->flags & WORD_OP_MASK) == WORD_OP_APPEND)
-				ms_copy(&flag_prefix[2], "(A)", 3);
-			if ((head_chain->flags & WORD_OP_MASK) == WORD_OP_HEREDOC)
-				ms_copy(&flag_prefix[2], "(H)", 3);
-			if (head_chain == head_group && head_group->next_chain == NULL)
-			{
-				print_fd(STDOUT_FILENO, "  [%s] '%a'\n", flag_prefix, head_chain->slice);
-				break ;
-			}
-			if (head_chain == head_group)
-				join_prefix = "╭ ";
-			else if (head_chain->next_chain == NULL)
-				join_prefix = "╰ ";
-			else
-				join_prefix = "│ ";
-			print_fd(STDOUT_FILENO, "%s[%s] '%a'\n", join_prefix, flag_prefix, head_chain->slice);
+			dump_word(head_group, head_chain);
 			head_chain = head_chain->next_chain;
 		}
 		head_group = head_group->next_group;
 	}
 }
 
-static int	run_builtin(char **argv, t_state *state)
-{
-	int	count;
-
-	count = (int) ms_ptrs_count(argv);
-	if (count == 0)
-		return (0);
-	if (ms_str_compare(argv[0], "cd", 0) == 0)
-		return (builtin_cd(count, argv, STDOUT_FILENO, state));
-	if (ms_str_compare(argv[0], "echo", 0) == 0)
-		return (builtin_echo(count, argv, STDOUT_FILENO, state));
-	if (ms_str_compare(argv[0], "env", 0) == 0)
-		return (builtin_env(count, argv, STDOUT_FILENO, state));
-	if (ms_str_compare(argv[0], "exit", 0) == 0)
-		return (builtin_exit(count, argv, STDOUT_FILENO, state));
-	if (ms_str_compare(argv[0], "export", 0) == 0)
-		return (builtin_export(count, argv, STDOUT_FILENO, state));
-	if (ms_str_compare(argv[0], "pwd", 0) == 0)
-		return (builtin_pwd(count, argv, STDOUT_FILENO, state));
-	if (ms_str_compare(argv[0], "unset", 0) == 0)
-		return (builtin_unset(count, argv, STDOUT_FILENO, state));
-	print_fd(STDERR_FILENO, "minishell: %s: command not found\n", argv[0]);
-	return (127);
-}
-
 static t_result	handle_line(char *line, t_state *state)
 {
 	t_word		*root_word;
-	char		**new_argv;
 	t_result	result;
 
 	result = word_chain_from_string(&root_word, slice0(line));
@@ -106,15 +98,12 @@ static t_result	handle_line(char *line, t_state *state)
 		return (result);
 	}
 	add_history(line);
-	new_argv = argv_from_word_group(root_word, &state->root_var);
-	dump_words(root_word);
+	result = expand_vars(root_word, state);
+	if (result == S_OK)
+		dump_words(root_word);
 	words_clr(&root_word);
 	free(line);
-	if (new_argv == NULL)
-		return (E_NOMEM);
-	//run_builtin(new_argv, state);
-	ms_ptrs_free(new_argv);
-	return (S_OK);
+	return (result);
 }
 
 int	main(int argc, char *argv[], char *envp[])
