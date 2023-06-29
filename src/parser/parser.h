fec5b28554bfc8aa59d19fabbe1ca0814b00db4c
jsprenge <jsprenge@student.42wolfsburg.de>
2023-06-29 22:20:23 +0200

    Make insert_vars private to the parser

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 18:55:33 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/06/29 22:18:08 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/06/29 22:18:52 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,9 +68,6 @@ typedef struct s_cmd
 	t_redir			*root_redir;
 	char			*argv[];
 }	t_cmd;
-
-// insert_vars.c
-t_result	insert_vars(t_word *head_group, t_state *state);
 
 // grammar.c
 size_t		begin_space(t_slice slice);
--- src/parser/private.h
+++ src/parser/private.h
@@ -6,7 +6,7 @@
 /*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 02:11:15 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/06/29 22:13:28 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/06/29 22:19:31 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,9 @@ typedef struct s_cmd_builder
 
 // cmd_builder_argv.c
 t_result	cmd_builder_build_argv(t_cmd_builder *self, t_word *root_word);
+
+// insert_vars.c
+t_result	insert_vars(t_word *head_group, t_state *state);
 
 // word_builder.c
 void		word_builder_group(t_word_builder *self);
