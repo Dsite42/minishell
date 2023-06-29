9de48e4aa709b23ce937c823a813e4f8831b4b68
jsprenge <jsprenge@student.42wolfsburg.de>
2023-06-29 21:26:52 +0200

    Make ignore_count group-level instead of chain-level in words_iter_cmd_args

--- src/parser/iter_cmd_args.c
+++ src/parser/iter_cmd_args.c
@@ -3,32 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   iter_cmd_args.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsprenge <jsprenge@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jsprenge <jsprenge@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 17:25:55 by jsprenge          #+#    #+#             */
-/*   Updated: 2023/06/27 21:05:34 by jsprenge         ###   ########.fr       */
+/*   Updated: 2023/06/29 21:26:00 by jsprenge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-// TODO: Check if command ends with operator which is invalid
-
 static t_result	iter_inner(void *context_ptr, t_word *head_group,
-	t_result (*callback)(void *context_ptr, int is_group, t_word *word))
+	t_result (*callback)(void *context_ptr, int is_group, t_word *word),
+	int *p_ignore_count)
 {
 	t_result	result;
 	t_word		*head_chain;
-	int			ignore_count;
 
-	ignore_count = 0;
 	head_chain = head_group;
 	while (head_chain != NULL)
 	{
 		if (head_chain->flags & WORD_IS_OP)
-			ignore_count = 2;
-		if (ignore_count > 0)
-			ignore_count--;
+			*p_ignore_count = 2;
+		if (*p_ignore_count > 0)
+			(*p_ignore_count)--;
 		else
 		{
 			result = callback(context_ptr,
@@ -46,12 +43,14 @@ t_result	words_iter_cmd_args(void *context_ptr, t_word **p_head_group,
 {
 	t_result	result;
 	t_word		*head_group;
+	int			ignore_count;
 
+	ignore_count = 0;
 	head_group = *p_head_group;
 	while (head_group != NULL
 		&& (head_group->flags & WORD_OP_MASK) != WORD_OP_PIPE)
 	{
-		result = iter_inner(context_ptr, head_group, callback);
+		result = iter_inner(context_ptr, head_group, callback, &ignore_count);
 		if (result != S_OK)
 			return (result);
 		head_group = head_group->next_group;
