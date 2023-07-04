b65c9193fcb8a7f2e4051559bf863157d1380859
cgodecke <cgodecke@student.42wolfsburg.de>
2023-07-04 15:27:30 +0200

    fixed echo.c. now also flag -nnnnn is interpreted as -n

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,11 +6,30 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 16:58:05 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/06/07 15:22:18 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/07/04 15:25:44 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+
+static int	is_minus_n(char *argv)
+{
+	int	i;
+
+	i = 0;
+	if (argv[i] == '-')
+	{
+		i++;
+		while (argv[i] != '\0')
+		{
+			if (argv[i] != 'n')
+				return (0);
+			i++;
+		}
+		return (1);
+	}
+	return (0);
+}
 
 int	builtin_echo(int argc, char *argv[], int out_fd, t_state *state)
 {
@@ -20,7 +39,7 @@ int	builtin_echo(int argc, char *argv[], int out_fd, t_state *state)
 	(void) state;
 	print_newline = 1;
 	index = 1;
-	while (argc >= 2 && ms_str_compare("-n", argv[index], 0) == 0)
+	while (argc >= 2 && argv[index] != NULL && is_minus_n(argv[index]) == 1)
 	{
 		print_newline = 0;
 		index++;
