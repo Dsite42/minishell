48ce2d327fe3ed0a5a7a9249363cfc7085e2f724
cgodecke <cgodecke@student.42wolfsburg.de>
2023-07-05 13:27:49 +0200

    use state_set_exit_code() now instet var_set()

--- src/execution/parent.c
+++ src/execution/parent.c
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 11:22:16 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/07/05 10:37:07 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/07/05 13:19:42 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,8 +59,8 @@ int	is_builtin(char **argv)
 
 int	parent(t_piping *piping_data, t_state *state)
 {
-	int		fd_dup[2];
-	char	*return_value;
+	int	fd_dup[2];
+	int	return_value;
 
 	if (*piping_data->cmd->argv != NULL
 		&& is_builtin(piping_data->cmd->argv) == 1
@@ -69,13 +69,18 @@ int	parent(t_piping *piping_data, t_state *state)
 		input_redirection(piping_data, fd_dup);
 		if (output_redirection(piping_data, fd_dup) == -1)
 		{
-			vars_set(&state->root_var, slice0("?"), slice0("1"));
+			if (state_set_exit_code(state, 1) == 0)
+			{
+				print_fd(2, "minishell: no free memory\n");
+				return (-1);
+			}
+		}
+		return_value = run_builtin(piping_data->cmd->argv, state);
+		if (state_set_exit_code(state, return_value) == 0)
+		{
+			print_fd(2, "minishell: no free memory\n");
 			return (-1);
 		}
-		return_value = ms_int_to_str(
-				run_builtin(piping_data->cmd->argv, state));
-		vars_set(&state->root_var, slice0("?"), slice0(return_value));
-		free(return_value);
 		if (dup2(state->saved_stdout, STDOUT_FILENO) == -1)
 			execution_error(1, "dup2_parent_output_error", 1, errno);
 		return (1);
