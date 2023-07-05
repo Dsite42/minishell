0d6b0c97ccf078349a5e09bd86a9f14104dd16ff
cgodecke <cgodecke@student.42wolfsburg.de>
2023-07-05 11:26:59 +0200

    unreadble redirect is now error handeled #34

--- src/execution/input_redirection.c
+++ src/execution/input_redirection.c
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 11:56:58 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/07/05 10:41:41 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/07/05 11:24:16 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 static int	is_read_or_heredoc(t_redir *root_redir)
 {
@@ -27,13 +28,16 @@ static int	is_read_or_heredoc(t_redir *root_redir)
 	return (0);
 }
 
-static void	dup_read(int *fd_dup, t_redir *head_redir)
+static void	dup_read(int *fd_dup, t_redir *head_redir, t_piping *piping_data)
 {
 	int	fd_infile;
 
 	fd_infile = open(head_redir->name, O_RDONLY);
 	if (fd_infile == -1)
-		execution_error(1, "input:", 1, errno);
+	{
+		execution_error(0, piping_data->cmd->root_redir->name, 1, errno);
+		exit(1);
+	}
 	fd_dup[0] = dup2(fd_infile, STDIN_FILENO);
 	if (fd_dup[0] == -1)
 		execution_error(1, "dup_infile_input_1 error", 1, errno);
@@ -49,16 +53,16 @@ static void	dup_read_heredoc(t_piping *piping_data, int *fd_dup)
 	while (head_redir != NULL)
 	{
 		if (head_redir->type == WORD_OP_READ)
-			dup_read(fd_dup, head_redir);
+			dup_read(fd_dup, head_redir, piping_data);
 		else if (head_redir->type == WORD_OP_HEREDOC
 			&& head_redir->last_heredoc == 1)
 		{
 			fd_infile = head_redir->pipefd_heredoc[0];
 			if (fd_infile == -1)
-				execution_error(1, "input:", 1, errno);
+				execution_error(1, "fd_error heredoc", 1, errno);
 			fd_dup[0] = dup2(fd_infile, STDIN_FILENO);
 			if (fd_dup[0] == -1)
-				execution_error(1, "dup_input_2 error", 1, errno);
+				execution_error(1, "dup_error heredoc", 1, errno);
 			close(fd_infile);
 		}
 		head_redir = head_redir->next;
@@ -74,9 +78,6 @@ void	input_redirection(t_piping *piping_data, int *fd_dup)
 		fd_dup[0] = dup2(piping_data->prev_read, STDIN_FILENO);
 		close(piping_data->prev_read);
 		if (fd_dup[0] == -1)
-		{
-			print_fd(2, "i:%i\n", piping_data->i);
-			execution_error(1, "dup_input_1 error", 1, errno);
-		}
+			execution_error(1, "dup_error prev_read", 1, errno);
 	}
 }
