7dd1c1b60d8a9877b46109d67bd0ac9211529615
cgodecke <cgodecke@student.42wolfsburg.de>
2023-07-05 13:34:00 +0200

    norminette in parent.c

--- src/execution/parent.c
+++ src/execution/parent.c
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 11:22:16 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/07/05 13:19:42 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/07/05 13:33:23 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,34 +57,38 @@ int	is_builtin(char **argv)
 	return (0);
 }
 
-int	parent(t_piping *piping_data, t_state *state)
+static int	builtin_in_parent(t_piping *piping_data, t_state *state)
 {
 	int	fd_dup[2];
 	int	return_value;
 
-	if (*piping_data->cmd->argv != NULL
-		&& is_builtin(piping_data->cmd->argv) == 1
-		&& piping_data->num_cmds == 1)
+	input_redirection(piping_data, fd_dup);
+	if (output_redirection(piping_data, fd_dup) == -1)
 	{
-		input_redirection(piping_data, fd_dup);
-		if (output_redirection(piping_data, fd_dup) == -1)
-		{
-			if (state_set_exit_code(state, 1) == 0)
-			{
-				print_fd(2, "minishell: no free memory\n");
-				return (-1);
-			}
-		}
-		return_value = run_builtin(piping_data->cmd->argv, state);
-		if (state_set_exit_code(state, return_value) == 0)
+		if (state_set_exit_code(state, 1) == 0)
 		{
 			print_fd(2, "minishell: no free memory\n");
 			return (-1);
 		}
-		if (dup2(state->saved_stdout, STDOUT_FILENO) == -1)
-			execution_error(1, "dup2_parent_output_error", 1, errno);
-		return (1);
 	}
+	return_value = run_builtin(piping_data->cmd->argv, state);
+	if (state_set_exit_code(state, return_value) == 0)
+	{
+		print_fd(2, "minishell: no free memory\n");
+		return (-1);
+	}
+	if (dup2(state->saved_stdout, STDOUT_FILENO) == -1)
+		execution_error(1, "dup2_parent_output_error", 1, errno);
+	return (1);
+}
+
+int	parent(t_piping *piping_data, t_state *state)
+{
+
+	if (*piping_data->cmd->argv != NULL
+		&& is_builtin(piping_data->cmd->argv) == 1
+		&& piping_data->num_cmds == 1)
+		return (builtin_in_parent(piping_data, state));
 	if (piping_data->i > 0)
 		close(piping_data->prev_read);
 	if (piping_data->i < piping_data->num_cmds - 1)
